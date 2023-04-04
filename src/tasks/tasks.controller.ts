a3d67c6ba40e50cf436b459d6baf0a892397c93c
Dirkooo <102775842+Dirkooo@users.noreply.github.com>
2023-04-04 19:32:47 -0400

    Update tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreatePruebaDto } from './dto/create-prueba.dto';
+import { privateDecrypt } from 'crypto';
+import { Model } from 'mongoose';
+import { Prueba } from './Entities/tasks.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { UpdatePruebaDto } from './dto/update-prueba.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+ 
+    constructor(private readonly TasksService: TasksService){} 
+
+    @Post()
+    create(@Body() CreatePruebaDto: CreatePruebaDto){
+        return this.TasksService.create(CreatePruebaDto);
+    }
+
+    @Get()
+    findAll(){
+        return this.TasksService.findall();
+    }
+
+    @Get(':id')
+    findOne(@Param(':id') id:string){
+        return this.TasksService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param(':id') id:string, @Body() UpdatePruebaDto: UpdatePruebaDto){
+        return this.TasksService.update(id, UpdatePruebaDto);
+    }
+
+    @Delete(':id')
+    remove(@Param(':id') id:string){
+        return this.TasksService.remove(id);
+    }
+    
