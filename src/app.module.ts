46f21d8dea34a754b7e308a63b14ee89e09138d4
Dirkooo <102775842+Dirkooo@users.noreply.github.com>
2023-04-04 19:35:30 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose'; 
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { ServeStaticModule } from '@nesjs/serve-static';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+  /*ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'),
+})],*/
+
+MongooseModule.forRoot('mongodb://localhost:27017/Todo'),
+  ]
 })
+
 export class AppModule {}
