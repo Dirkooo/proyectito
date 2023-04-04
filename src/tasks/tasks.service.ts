85bd90917603099e70de5b2f073be0b27f12383b
Dirkooo <102775842+Dirkooo@users.noreply.github.com>
2023-04-04 19:31:19 -0400

    Update tasks.service.ts

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService { constructor(
+        @InjectModel(Prueba.name)
+        private readonly PruebaModel: Model<Prueba>,
+    ){}
+    async create(CreatePruebaDto: CreatePruebaDto){
+        CreatePruebaDto.name = CreatePruebaDto.name.toLocaleLowerCase();
+        const prueba = await this.PruebaModel.create(CreatePruebaDto);
+        return prueba;
+    }
+
+   async findall(){
+        return this.PruebaModel.find();
+    }
+
+    async findOne(id: string){
+        let prueba: Prueba;
+        if(!isNaN(+id)){
+            prueba = await this.PruebaModel.findOne({no: id});
+        }
+
+        if(isValidObjectId(id)){
+            prueba = await this.PruebaModel.findById(id);
+        }
+        if(!prueba && isValidObjectId(id)){
+            prueba = await this.PruebaModel.findById(id);
+        }
+
+        if(!prueba) {throw new NotFoundException ('No encontrado')};
+
+
+
+        return prueba;
+
+    }
+
+    async update(id: string, UpdatePruebaDto: UpdatePruebaDto){
+        const prueba = await this.findOne(id);
+        if(UpdatePruebaDto.name){
+            UpdatePruebaDto.name = UpdatePruebaDto.name.toLocaleLowerCase();
+
+            await prueba.updateOne(UpdatePruebaDto, {new: true});
+        } 
+        return {...prueba.toJSON(), ...UpdatePruebaDto};
+    }
+
+    async remove(id :string){
+       const prueba = await this.PruebaModel.findByIdAndDelete(id);
+        return 200;
+    }
+
+}
