24bf8ba64b46c2d7fce32cdf3a929e22f0131409
Dirkooo <102775842+Dirkooo@users.noreply.github.com>
2023-04-04 19:32:21 -0400

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Prueba, PruebaSchema } from './Entities/tasks.entity';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
+  imports: [
+    MongooseModule.forFeature([{
+      name: Prueba.name,
+      schema: PruebaSchema,
+    }])
+  ]
 })
 export class TasksModule {}
